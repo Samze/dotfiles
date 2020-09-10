6d87de35fca98660abe8d2353087ef975d60b391
samze <samgzeit@gmail.com>
2020-09-10 20:54:58 +0100

    Update brew installation

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ log() {
 
 install_ansible() {
   log "Installing/upgrading Homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   brew install ansible
 }
 
